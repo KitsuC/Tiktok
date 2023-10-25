8575fa1386d30e45ea71d9d752fe1c1bb13e7f98
KitsuC <1102000quan22@gmail.com>
2023-10-25 11:24:48 +0700

    Xây dựng UI phần Header #1

---
+++ src/assets/images/index.js
@@ -0,0 +1,12 @@
+const images = {
+  logo: require('~/assets/images/svg/logo.svg').default,
+  search: require('~/assets/images/svg/search.svg').default,
+  app: require('~/assets/images/svg/app.svg').default,
+  notif: require('~/assets/images/svg/notif.svg').default,
+  plus: require('~/assets/images/svg/plus.svg').default,
+  mess: require('~/assets/images/svg/mess.svg').default,
+  close: require('~/assets/images/svg/close.svg').default,
+  load: require('~/assets/images/svg/load.svg').default,
+};
+
+export default images;
